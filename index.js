4c4e5b034f7e2cd781d7c06ef363a0f9af90198f
Aditya Kumar <adityakr80510@gmail.com>
2022-08-08 01:37:06 +0530

    better

---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var { createWorker } = require("tesseract.js");
+
+const app = express();
+
+// for parsing json data
+app.use(express.json());
+
+// for parsing multiform data
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.use("/public", express.static(path.join(__dirname + "/public")));
+app.use(express.static(__dirname + "/"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./index.html"));
+});
+
+// api Routes
+app.use("/api", (req, res) => {
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+  });
+
+  (async () => {
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text },
+    } = await worker.recognize(
+      `https://onlinetestseries.motion.ac.in/dashboard/img/testid-66665555137/${
+        req.body.Q ? "Question" : "Solution"
+      }_${req.body.ques}.jpg`
+    );
+    var newtext;
+    if (!text.includes("Sol:")) {
+      newtext = text.replace("Answer:", "").trim();
+    } else {
+      newtext = text.split("Sol:")[0].replace("Answer:", "").trim();
+    }
+    res.send(newtext);
+    await worker.terminate();
+  })();
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(
+    `listen started on ${process.env.PORT ? process.env.PORT : "8080"}`
+  );
+});
